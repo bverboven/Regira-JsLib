68c5703c80be099f7d802e48a88a7eb3664abfb6
bverboven <bramverboven@hotmail.com>
2024-04-11 23:21:27 +0200

    smarter anchor

--- src/vue/ui/input/Anchor.vue
+++ src/vue/ui/input/Anchor.vue
@@ -4,6 +4,7 @@
 
 <script setup lang="ts">
 import { computed } from "vue"
+import { isEmail, isPhone } from "@/regira_modules/utilities/string-utility"
 
 const props = defineProps<{
     href: string
@@ -11,7 +12,15 @@ const props = defineProps<{
 
 const url = computed(() => {
     let url = props.href
-    if (!url.startsWith("http") && !["mailto:", "tel:", "ftp:"].some((prefix) => url.startsWith(prefix))) {
+    if (isEmail(url)) {
+        if (!url.startsWith("mailto:")) {
+            url = "mailto:" + url
+        }
+    } else if (isPhone(url)) {
+        if (!url.startsWith("tel:")) {
+            url = "tel:" + url
+        }
+    } else if (!url.startsWith("http") && !["mailto:", "tel:", "ftp:"].some((prefix) => url.startsWith(prefix))) {
         url = "http://" + url
     }
     return url
