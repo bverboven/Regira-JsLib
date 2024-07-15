c857f31991317d07240e308bd3355f0fa0ad8b0d
bverboven <bramverboven@hotmail.com>
2024-07-15 17:04:20 +0200

    navbar grouped

--- src/vue/entities/navigation/abstractions/types.ts
+++ src/vue/entities/navigation/abstractions/types.ts
@@ -1,15 +1,12 @@
 export interface INavCore {
     id: string
-    title: string
     parentId?: string
-}
-export interface IDescribingNavItem {
+    title: string
+    shortTitle?: string
     icon: string
-    description?: String
 }
 export interface IRoutingNavItem {
     routeName: string
     initialQuery?: Record<string, any>
 }
-export interface INavItem extends INavCore, IDescribingNavItem, IRoutingNavItem {
-}
+export interface INavItem extends INavCore, IRoutingNavItem {}
--- src/vue/entities/navigation/functions.ts
+++ src/vue/entities/navigation/functions.ts
@@ -1,17 +1,16 @@
-import { computed, ref } from "vue"
 import { TreeList } from "../../../treelist"
 import type { IConfig } from "../abstractions"
 import type { INavItem, INavCore } from "./abstractions/types"
 import NavGroup from "./NavGroup"
 import NavItem from "./NavItem"
 
-export function createNavGroup(input: { id: string; title: string }): INavCore {
+export function createNavGroup(input: { id: string; title: string; icon: string }): INavCore {
     return Object.assign(new NavGroup(), input)
 }
 export function createNavItem(input: IConfig, parentId?: string): INavItem {
     return Object.assign(new NavItem(), {
         id: input.key,
-        parentId: parentId ?? input.group,
+        parentId: parentId,
         icon: input.name,
         routeName: `${input.name}Overview`,
         title: input.overviewTitle,
@@ -20,9 +19,10 @@ export function createNavItem(input: IConfig, parentId?: string): INavItem {
     })
 }
 
+type IGroupedEntities = [string, Array<string>]
 type IImportDashboardInput = {
-    groups: Array<{ id: string; title: string }>
-    entities: Array<{ group: string; items: Array<string> }>
+    groups: Array<{ id: string; title: string; icon: string }>
+    entities: Array<IGroupedEntities>
     configs: Array<IConfig>
     hasAccess: (config: IConfig) => boolean
 }
@@ -32,7 +32,7 @@ export function importDashboard(input: IImportDashboardInput): Array<INavCore> {
     }
 
     const navGroups = input.groups!.map((x) => createNavGroup(x))
-    const navEntities = input.entities!.flatMap(({ group: parentId, items }) =>
+    const navEntities = input.entities!.flatMap(([parentId, items]) =>
         items
             .map((x) => findEntityConfig(x)!)
             .filter((config) => input.hasAccess(config))
@@ -41,29 +41,35 @@ export function importDashboard(input: IImportDashboardInput): Array<INavCore> {
     return navGroups.concat(navEntities)
 }
 type IImportNavbarInput = {
-    entities: Array<string>
+    groups?: Array<{ id: string; title: string; icon: string }>
+    entities: Array<string | IGroupedEntities>
     configs: Array<IConfig>
     hasAccess: (config: IConfig) => boolean
 }
-export function importNavbar(input: IImportNavbarInput) {
+export function importNavbar(input: IImportNavbarInput): Array<INavCore> {
     function findEntityConfig(key: string): IConfig | undefined {
         return input.configs.find((x) => x.key == key)
     }
-    return input
-        .entities!.map((x) => findEntityConfig(x)!)
-        .filter((config) => input.hasAccess(config))
-        .map((config) => createNavItem(config))
+    const groups = input.groups?.map(createNavGroup)
+    return input.entities!.flatMap((x) => {
+        if (x.length == 2 && Array.isArray(x[1])) {
+            const group = groups!.find((g) => g.id == x[0])!
+            const navItems = x[1]
+                .map((x) => findEntityConfig(x)!)
+                .filter((config) => input.hasAccess(config))
+                .map((config) => createNavItem(config, group.id))
+            return [group, ...navItems]
+        }
+        // else
+        const config = findEntityConfig(x as string)!
+        return input.hasAccess(config) ? [createNavItem(config)] : []
+    })
 }
 
 export function buildNavigationTree(items: Array<INavCore>): TreeList<INavCore> {
     return new TreeList<INavCore>().init(items, (value, candidates) => candidates.filter((x) => x.id == value.parentId))
 }
 
-const entities = ref<Array<INavCore>>()
-export function useNavigationTree(items: Array<INavCore>) {
-    entities.value = items
-    const tree = computed<TreeList<INavCore>>(() => buildNavigationTree(entities.value!))
-    return {
-        tree,
-    }
+export function isNavItem(item: INavCore) {
+    return item instanceof NavItem
 }
