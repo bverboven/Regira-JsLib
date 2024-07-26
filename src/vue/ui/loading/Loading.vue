3a3a36e061b3e5dc7a1283e5a32044d46e4b9953
bverboven <bramverboven@hotmail.com>
2024-07-26 09:57:12 +0200

    loading img dimensions

--- src/vue/ui/loading/Loading.vue
+++ src/vue/ui/loading/Loading.vue
@@ -1,9 +1,16 @@
 <template>
-  <img :src="img" />
+    <img :src="img" ref="imgEl" />
 </template>
 
 <script setup lang="ts">
-import { inject } from "vue";
+import { ref, inject } from "vue"
 
-const img = inject<string>("loadingImg");
+const img = inject<string>("loadingImg")
+const imgEl = ref<InstanceType<typeof HTMLImageElement> | null>(null)
+
+defineExpose({
+    imgEl,
+    dimensions: () => [imgEl.value?.width, imgEl.value?.height],
+    height: () => imgEl.value?.naturalHeight,
+})
 </script>
