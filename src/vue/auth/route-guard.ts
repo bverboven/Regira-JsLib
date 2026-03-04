f46d6a091aa59ca549763b7e94942ff9970842a8
Bram Verboven <bram@regira.com>
2026-03-04 22:20:46 +0100

    fix warning route next

--- src/vue/auth/route-guard.ts
+++ src/vue/auth/route-guard.ts
@@ -2,30 +2,30 @@ import type { Store } from "pinia"
 import type { Router } from "vue-router"
 
 export default ({ router, store }: { router: Router; store: Store & { isAuthenticated: boolean; hasPermission(value: string): boolean } }) => {
-    router.beforeEach((to, from, next) => {
+    router.beforeEach((to, from) => {
         // allowAnonmyous
         if (to.meta && to.meta.allowAnonymous) {
-            return next()
+            return true
         }
 
         const isAuthenticated = store.isAuthenticated
         if (isAuthenticated) {
             const policies = to.matched.map((r) => r.meta?.policy as (store: Store) => boolean).filter((p) => typeof p == "function")
             if (policies.length && !policies.every((p) => p(store))) {
-                return next({ name: "forbidden", query: { url: to.fullPath } })
+                return { name: "forbidden", query: { url: to.fullPath } }
             }
 
             const requiredPermissions = to.matched.flatMap((r) => (r.meta?.permissions as Array<string>) || [])
 
             if (requiredPermissions.length && !requiredPermissions.every((r) => store.hasPermission(r))) {
-                return next({ name: "forbidden", query: { url: to.fullPath } })
+                return { name: "forbidden", query: { url: to.fullPath } }
             }
 
-            return next()
+            return true
         }
 
         store.$patch({ authRequired: true })
-        return next()
-        //return next({ name: "login", query: { returnUrl: to.query.returnUrl || to.fullPath } })
+        return true
+        //return { name: "login", query: { returnUrl: to.query.returnUrl || to.fullPath } }
     })
 }
